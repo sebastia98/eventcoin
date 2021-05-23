234c6af7890380314660c1d7ae24a356f7decd16
Sebas Adroevr <sebastia.adrover@melia.com>
2021-05-23 22:09:14 +0200

    feat: plantillas páginas LogIn y SignUp

---
+++ src/pages/LogIn/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function LogIn() {
+    return (
+        <div>
+            <h1>Este es el fichero de log in</h1>
+            <form>
+
+            </form>
+        </div>
+    )
+}
+
+export default LogIn;
