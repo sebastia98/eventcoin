bf8389bf1dd28823d94628a151181bac7520ecdd
Sebas Adroevr <sebastia.adrover@melia.com>
2021-05-24 15:50:14 +0200

    Feat: Añadir className a las tags

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -10,25 +10,25 @@ function Card({event}) {
             <div className="card-event">
                 <div className="card-event-img" style={{backgroundImage:`url(${event?.image})`}}/>
                 <div className="card-event-description">
-                    <p>
+                    <p className="card-name">
                         {event?.name}
                     </p>
-                    <p>
+                    <p className="card-date">
                         {event?.date}
                     </p>
-                    <p>
+                    <p className="card-description">
                         {event?.description}
                     </p>
                     <div className="event-tickets">
                         <div className="event-ticket-wrapper">
                             <i className="fas fa-ticket-alt"></i>
-                            <p>
+                            <p className="card-price">
                                 {event?.price}
                             </p>
                         </div>
                         <div className="event-ticket-wrapper">
                             <i className="fas fa-money-check"></i>
-                            <p>
+                            <p className="card-tickets">
                                 {event?.availableTickets}
                             </p>
                         </div>
