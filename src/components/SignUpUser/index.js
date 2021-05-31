bcfb6c9c403d7c07beb868c4f291ab4bc94b840c
Sebas Adroevr <sebastia.adrover@melia.com>
2021-05-31 21:53:28 +0200

    Feat: Peticiones al backend

--- src/components/SignUpUser/index.js
+++ src/components/SignUpUser/index.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './index.css';
 
 function SignUpUser() {
+
+    // const eventHandler = () => {
+        
+    //     fetch("http://localhost:8080/insertuser", 
+    //     {method: "POST",
+    //     body: JSON.stringify(event)}).then(data => {
+    //         console.log(data)
+    //     })
+    //     console.log(event);
+    // }
+
+    const eventValidation = () => {
+        console.log(event);
+    }
+
+    const [event, setEvent] = useState({}); 
 
     return (
         <div className="container">
@@ -10,33 +26,51 @@ function SignUpUser() {
                 <div className="user-details">
                     <div className="input-box">
                         <span className="details">Full Name</span>
-                        <input type="text" placeholder="Enter your Name" required/>
+                        <input type="text" placeholder="Enter your Name" required onChange={(e) => {
+                            setEvent({...event, name: e.target.value})
+                        }}/>
                     </div>
                     <div className="input-box">
                         <span className="details">Username</span>
-                        <input type="text" placeholder="Enter your Nick" required/>
+                        <input type="text" placeholder="Enter your Nick" required onChange={(e) => {
+                            setEvent({...event, username: e.target.value})
+                        }}/>
                     </div>
                     <div className="input-box">
                         <span className="details">Email</span>
-                        <input type="email" placeholder="Enter your Email" required/>
+                        <input type="email" placeholder="Enter your Email" required onChange={(e) => {
+                            setEvent({...event, email: e.target.value})
+                        }}/>
                     </div>
                     <div className="input-box">
                         <span className="details">Phone Number</span>
-                        <input type="tel" placeholder="Enter your Number" required />
+                        <input type="tel" placeholder="Enter your Number" required onChange={(e) => {
+                            setEvent({...event, phoneNumber: e.target.value})
+                        }}/>
                     </div>
                     <div className="input-box">
                         <span className="details">Password</span>
-                        <input type="password" placeholder="Enter your Password" required/>
+                        <input type="password" placeholder="Enter your Password" required onChange={(e) => {
+                            setEvent({...event, password: e.target.value})
+                        }}/>
                     </div>
                     <div className="input-box">
                         <span className="details">Confirm Password</span>
-                        <input type="password" placeholder="Enter your Password" required/>
+                        <input type="password" placeholder="Repeat your Password" required onChange={(e) => {
+                            setEvent({...event, confirmPassword: e.target.value})
+                        }}/>
                     </div>
                 </div>
                 <div className="gender-details">
-                    <input type="radio" name="gender" id="dot-1"/>
-                    <input type="radio" name="gender" id="dot-2"/>
-                    <input type="radio" name="gender" id="dot-3"/>
+                    <input type="radio" name="gender" id="dot-1" onChange={(e) => {
+                            setEvent({...event, gender: "male"})
+                        }}/>
+                    <input type="radio" name="gender" id="dot-2" onChange={(e) => {
+                            setEvent({...event, gender: "female"})
+                        }}/>
+                    <input type="radio" name="gender" id="dot-3" onChange={(e) => {
+                            setEvent({...event, gender: "not binary"})
+                        }}/>
                     <span className="gender-title">Gender</span>
                     <div className="category">
                         <label htmlFor="dot-1">
@@ -54,10 +88,12 @@ function SignUpUser() {
                     </div>
                 </div>
                 <div className="button">
-                    <input type="submit" value="Register"/>
+                    <input type="submit" value="Register" onClick={eventValidation}/>
                 </div>
             </form>
         </div>
+ 
+        // TODO: hacer peticion fetch en el eventHandler
     )
 }
 
