0c46120d4fa16cba549bb8c927fab63a768372ab
Sebas Adroevr <sebastia.adrover@melia.com>
2021-06-08 15:28:51 +0200

    Feat: Añadir tag para linkear el Service Form

--- src/components/ServiceForm/index.js
+++ src/components/ServiceForm/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import { FORM_SERVICE_URL } from '../../utils/urls';
 import './index.css';
 
 const ServiceForm = () => {
     return (
         <div className="service-form-container">
             <div className="button-container">
-                <button className="button-service">Add a new service</button>
+                <Link to={FORM_SERVICE_URL}>
+                    <button className="button-service">Add a new service</button>
+                </Link>
             </div>
             <form className="service-form">
                 <input type="text" placeholder="Type your search"/>
