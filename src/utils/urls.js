978117d2a31394a16953600f9ada18a9489618c5
Sebas Adroevr <sebastia.adrover@melia.com>
2021-06-08 15:29:50 +0200

    Feat: Añadir form-service a la ruta

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -3,4 +3,5 @@ export const SERVICES_URL = "/services";
 export const ACCOUNT_URL = "/account";
 export const SIGN_UP_URL = "/sign-up";
 export const LOG_IN_URL = "/log-in";
-export const EVENT_URL = "/event"
+export const EVENT_URL = "/event";
+export const FORM_SERVICE_URL = "/form-service";
