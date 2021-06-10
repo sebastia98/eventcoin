ca77e0509b6ef6ad3c6964295f628e1b7e9f5964
Sebas Adroevr <sebastia.adrover@melia.com>
2021-06-11 00:07:46 +0200

    Feat: Petición para login

--- src/components/LogInUser/index.js
+++ src/components/LogInUser/index.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './index.css';
 
 function LogInUser() {
+
+    const sendCredentials = () => {
+        new Promise((resolve, reject) => {
+            fetch("/user/loginUser", {
+            method: "POST",
+            headers: {
+                "Access-Control-Allow-Origin" : "*",
+                "Content-Type": "application/json"},
+            body: JSON.stringify(user)
+        })
+        .then(response => resolve(response))
+        .catch(error => reject(error))
+        })
+    };
+
+    const logUser = () => {
+        sendCredentials()
+            .then(response => console.log(response))
+            .catch(error => console.log(error))
+            .finally(() => console.log("se acabó"))
+    };
+
+    const [user, setUser] = useState({});
 
     return (
         <div className="container">
@@ -10,15 +33,17 @@ function LogInUser() {
                 <div className="user-details">
                     <div className="input-box">
                         <span className="details">Username</span>
-                        <input type="text" placeholder="Enter your Nick" required/>
+                        <input type="text" placeholder="Enter your Nick" required onChange={(event) => {
+                            setUser({...user, username : event.target.value})}}/>
                     </div>
                     <div className="input-box">
                         <span className="details">Password</span>
-                        <input type="password" placeholder="Enter your Password" required/>
+                        <input type="password" placeholder="Enter your Password" required onChange={(event) => {
+                            setUser({...user, password : event.target.value})}}/>
                     </div>
                 </div>
                 <div className="button">
-                    <input type="submit" value="LogIn"/>
+                    <input type="submit" value="LogIn" onClick = {logUser}/>
                 </div>
             </form>
         </div>
