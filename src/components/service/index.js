cb836e422de1f6fc054ee944b2bb739b5ba14235
Sebas Adroevr <sebastia.adrover@melia.com>
2021-06-11 16:54:08 +0200

    Feat: settear campos del user

--- src/components/service/index.js
+++ src/components/service/index.js
@@ -4,8 +4,8 @@ import './index.css';
 const Service = ({service}) =>  {
     return (
         <tr className="service-container">
-            <td className="user-name">{service?.username}</td>
-            <td className="full-name">{service?.fullName}</td>
+            <td className="user-name">{service?.userId.username}</td>
+            <td className="full-name">{service?.userId.fullName}</td>
             <td className="offered-services">{service?.offeredServices}</td>
             <td className="service-price">{service?.rate} €</td>
         </tr>
