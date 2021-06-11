1d6e04b6556712f8ee9fd904f1521a80826da463
Sebas Adroevr <sebastia.adrover@melia.com>
2021-06-11 18:49:28 +0200

    Feat: Implemenar useHistory

--- src/pages/FormService/index.js
+++ src/pages/FormService/index.js
@@ -1,19 +1,17 @@
 import React, {useState} from 'react';
+import {useHistory} from 'react-router-dom';
+import { SERVICES_URL } from '../../utils/urls';
 import './index.css';
 
 export const FormService = () => {
-
-    const [event, setEvent] = useState({});
 
     const processServiceData = (e) => {
 
         e.preventDefault();
 
-        sendService().then((response) => {
-            console.log(response);
-        }).catch((error) => {
-            console.log(error);
-        });
+        sendService()
+            .then(() => history.push(SERVICES_URL))
+            .catch(error => setError(error));
     }
 
     const sendService = () => 
@@ -24,45 +22,45 @@ export const FormService = () => {
                             "access-control-allow-origin" : "*",
                             "Content-Type": "application/json"
                         },
-                body: JSON.stringify({...event, userId : "60be367dff78f742d2a171e8"})
+                body: JSON.stringify({...service, userId : JSON.parse(sessionStorage.getItem("userLogged"))._id})
                 })
-                .then((response) => {
-                    resolve(response);
-                })
-                .catch((error) => {
-                    reject(error);
-                })
+                .then(response => resolve(response))
+                .catch(error => reject(error))
         });
     
+    const history = useHistory();
+    const [service, setService] = useState({});
+    const [error, setError] = useState();
 
     return (
         <div className="service-form-wrapper">
             <div className="container">
-           <div className="title">Register your service</div>
+            <div className="title">Register your service</div>
+            {error && <label>Have occurred an error. Try again!</label>}
             <form>
                 <div className="service-details">
                     <div className="input-box">
                         <span className="details">What do you offer?</span>
                         <input type="text" placeholder="Offered services" required onChange={(e) => {
-                            setEvent({...event, offeredServices: e.target.value})
+                            setService({...service, offeredServices: e.target.value})
                         }}/>
                     </div>
                     <div className="input-box">
                         <span className="details">What is your rate for hour?</span>
                         <input type="number" placeholder="Specify your rate" required onChange={(e) => {
-                            setEvent({...event, rate: e.target.value})
+                            setService({...service, rate: e.target.value})
                         }}/>
                     </div>
                     <div className="input-box">
                         <span className="details">What are your references?</span>
                         <input type="text" placeholder="Place where you have worked" required onChange={(e) => {
-                            setEvent({...event, references: e.target.value})
+                            setService({...service, references: e.target.value})
                         }}/>
                     </div>
                     <div className="input-box">
                         <span className="details">Describes you know to do</span>
                         <textarea className="input-textarea" type="textarea" placeholder="Type your experencies and you think about you could contribute" required onChange={(e) => {
-                            setEvent({...event, description: e.target.value})
+                            setService({...service, description: e.target.value})
                         }}/>
                     </div>
                 </div>
