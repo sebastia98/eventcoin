ebaa817459cc633e36df81023926c4b539576587
Sebas Adroevr <sebastia.adrover@melia.com>
2021-06-13 14:44:46 +0200

    Feat: Implementar options de service para user

--- src/components/service/index.js
+++ src/components/service/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './index.css';
 
-const Service = ({service}) =>  {
+const Service = ({service, userService}) =>  {
     return (
         <tr className="service-container">
             <td className="user-name">{service?.userId.username}</td>
             <td className="full-name">{service?.userId.fullName}</td>
             <td className="offered-services">{service?.offeredServices}</td>
             <td className="service-price">{service?.rate} €</td>
+            {userService && <td className = "user-options"><button className = "edit">Edit</button><button className = "delete">Delete</button></td>}
         </tr>
     )
 }
--- src/pages/Account/index.js
+++ src/pages/Account/index.js
@@ -1,15 +1,58 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import Service from '../../components/service';
+import {Button} from '../../components/Button';
 import "./index.css";
 
 function Account() {
 
+    const userLogged = JSON.parse(sessionStorage.getItem("userLogged"));
+
+    const [data, setData] = useState([]);
+
+    const readUserServices = () => {
+        fetch("/service/readServices", {
+            method: "GET",
+            headers: {
+                "access-control-allow-origin" : "*",
+                "Content-Type": "application/json"},
+        })
+        .then(response => response.json())
+        .then(responseData => {
+            setData(responseData.serv)})
+        .catch(error => console.log(error))
+    };
+
+    useEffect(() => {
+        readUserServices()
+    }, []);
+
     const notLoggedContainer = <div className = "logged-container">
-                                    <h1>User not logged</h1>
+                                    <p className = "welcome">Please register or login</p>
+                                    <div className = "buttom-container">
+                                        {<Button buttonStyle='btn--outline' path="/sign-up">SIGN UP</Button>}
+                                        {<Button buttonStyle='btn--outline' path="/log-in">LOG IN</Button>}
+                                    </div>
                                 </div>;
 
     const loggedContainer = <div className = "logged-container">
-        <h1>User logged</h1>
-    </div>
+                                <p className = "welcome">Welcome sr. {userLogged && userLogged.fullName}</p>
+                                <div className = "table-container">
+                                <table className = "table">
+                                    <thead className = "table-header">
+                                        <tr>
+                                            <th>Nick</th>
+                                            <th>Full name</th>
+                                            <th>Offered services</th>
+                                            <th>Rate</th>
+                                            <th className = "options"></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody className="table-body">    
+                                        {data.filter(serv => JSON.stringify(serv.userId) === JSON.stringify(userLogged)).map((service, index) => <Service key = {index} service = {service} userService = {true}></Service>)}
+                                    </tbody>
+                                </table>
+                                </div>
+                            </div>
 
 
 
