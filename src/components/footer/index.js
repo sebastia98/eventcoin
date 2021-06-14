23de9811bdebc5ea4e076b3088dbad465f51689c
Sebas Adroevr <sebastia.adrover@melia.com>
2021-06-14 11:03:12 +0200

    Feat: Implementar footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+export const Footer = () => {
+    return (
+        <div className = "footer-container">
+            <footer className = "footer">
+                <p>Welcome to Service Web</p>
+                <p>Developed by Sebastià Adrover, Copyright</p>
+            </footer>
+        </div>
+    )
+}
