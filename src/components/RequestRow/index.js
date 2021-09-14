5004c39743257f5183719b4b2839bb8566230a14
Sebas Adrover <sebastia.adrover@melia.com>
2021-09-14 19:27:12 +0200

    Feat: confirm user

--- src/components/RequestRow/index.js
+++ src/components/RequestRow/index.js
@@ -1,13 +1,23 @@
+import React from 'react'
 import './index.css'
 
 export const RequestRow = (props) => {
+    
+    const checkOwnerConfirmed = props?.participation === "owner" && props?.request.ownerState === "done"
+
+
+    const checkApplicantConfirmed =  props?.participation === "applicant" && props?.request.applicantState === "done"
+
+    const checkParticipation = ( checkOwnerConfirmed || checkApplicantConfirmed) ?
+            <p>Confirmed</p> :
+            <button className="button-confirm" onClick = {() => {props.confirmRequest(props.request._id)}}>Confirm</button>
+
     return (
         <tr className = "request-row">
-            <td>{props?.request.serviceId.offeredServices}</td>
+            <td>{props?.request.serviceId?.offeredServices}</td>
             <td>{props?.request.dateRequestService.substring(0, 10)}</td>
             <td>{props?.request.startRequestService + " - " + props?.request.endRequestService}</td>
-            <td><button className="button-confirm" onClick = {() => {
-                props.confirmRequest(props.request._id)}}>Confirm</button></td>
+            <td>{checkParticipation}</td>
         </tr>
     )
 }
