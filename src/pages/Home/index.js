43d7adbb0f41f1c32f6953e2d1c98f17ba7ece24
Sebas Adrover <sebastia.adrover@melia.com>
2021-10-06 12:34:39 +0200

    Feat: Implements services filter

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,8 +6,9 @@ import { TableServices } from '../../components/TableServices'
 export const Home = () => {
 
     const [data, setData] = useState([]);
+    const [filter, setFilter] = useState("");
 
-    const readServices = () => {
+    const readServices = (filter) => {
         fetch("/service/readServices", {
             method: "GET",
             headers: {
@@ -16,18 +17,33 @@ export const Home = () => {
             
         })
         .then(response => response.json())
-        .then(responseData => {setData(responseData.serv)})
+        .then(responseData => filterServices(filter, responseData.serv))
         .catch(error => console.log(error))
     };
 
+    const filterServices = (filter, services) => {
+        if(filter) {
+            const filteredServices = services.filter(service => service.userId.username.toLowerCase().includes(filter) || 
+                                                                service.userId.fullName.toLowerCase().includes(filter) || 
+                                                                service.offeredServices.toLowerCase().includes(filter) ||
+                                                                service.references.toLowerCase().includes(filter))
+            setData(filteredServices)
+        } else {
+            setData(services)
+        }
+    }
+
     useEffect(() => {
-        readServices()
-    }, []);
+        readServices(filter)
+    }, [filter]);
 
     return (
         <div className="home-page">
             <div className = "services-container">
                 <p className = "available-services">Available services</p>
+                <div className = "input-container">
+                    <input className = "input-filter" type = "text" placeholder = "Filter services" onChange = {(e) => {setFilter(e.target.value)}}></input><i class="fas fa-search"></i>
+                </div>
                 {data.length === 0 ? <p>There aren't available services</p> : <TableServices data = {data}></TableServices>}
             </div>
         </div>
